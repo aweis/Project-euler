a7a8a5c939f98096d7bd473ec5afdb1d3457a473
Adam Weis <aweis@andrew.cmu.edu>
2011-12-29 21:48:00 -0500

    More documentation

--- ruby.rb
+++ ruby.rb
@@ -1,3 +1,4 @@
+#Find the sum of all the multiples of 3 or 5 below 1000
 def one
   acc = 0
   for i in 1...1000
@@ -8,6 +9,7 @@ def one
   acc
 end
 
+#Find the sum of the even fibonacci numbers from 1..4 million
 def two
   first = 1
   second = 2
@@ -28,6 +30,7 @@ def two
   acc
 end
 
+#What is the largest prime factor of the number 600851475143
 def three num
   max = 0
   acc = 1
@@ -50,6 +53,7 @@ def three num
 end
 
 #this is the solution to euler 3 except using my prime factorization method as a helper
+
 def three_b num
   a = prime_factorization num
   return a[a.size-2]
@@ -121,6 +125,8 @@ def isPalindrome_helper numls
   end
 end
 
+#takes in a number and an array, returns the number split into array cells
+#123 => [1,2,3]
 def numTo_a(num, ls)
   if num == 0
     ls
@@ -130,7 +136,8 @@ def numTo_a(num, ls)
   end
 end
 
-
+#What is the smallest positive number that is evenly divisible by all
+#numbers from 1 to 20
 def five
   i = 1
   begin
@@ -153,6 +160,7 @@ def div_n(n, num)
   return true
 end
 
+#What is the difference between the sum of the squares and the square of the sums of 1..100
 def six
   square_of_sum(100) - sum_of_squares(100)
 end
@@ -203,6 +211,7 @@ def find_n_primes n
   return acc
 end
 
+#Find the greatest product of 5 consecutive digits in the 1000 digit number
 def eight 
   max = 0
   number = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
@@ -220,6 +229,7 @@ def eight
   return max
 end
 
+#Find the only pythagorean triplet {a,b,c} for which a + b + c = 1000
 def nine
   acc = 1
   list_pythagorean.split(" ").map {|x| x.split('=')[1]}.each do |i| 
@@ -245,6 +255,7 @@ def is_a_square? n
   Math.sqrt(n).truncate.to_f == Math.sqrt(n)
 end
 
+#calculate the sum of all the primes below 2 million
 def ten
   acc = 0
   (find_primes_below 2000000).each do |i|
@@ -265,6 +276,7 @@ def find_primes_below n
   return acc
 end
 
+#what is the greatest product of 4 adjacent numbers on the same straight line in the given 20x20 grid
 def eleven
   max = 0
   matrix = read_file
@@ -404,49 +416,7 @@ def count_factors num
   return 2*count
 end
 
-#what is the value of the first triangle number to have over 500 divisors
-def twelve
-  iter = 1
-  trinum_temp = 0
-  begin
-    factors = count_factors(trinum_temp = (trinum(iter)))
-    iter = iter + 1
-
-  end until factors > 200
-
-  return trinum_temp
-end
-
-#first did recursion, then optimized slightly to iteration because needed speed
-def trinum n
-  temp = 0
-  for i in 1..n
-    temp = temp + i
-  end
-  return temp
-end
-
-#needed optimization to finish before 1 minutes threshold
-def count_factors num
-  count = 0
-  if num == 1
-    return 1
-  end
-
-  lim = Math.sqrt(num)
-  for i in 1...lim
-    if num % i == 0
-      count = count + 1
-    end
-  end
-
-  if is_a_square?(num)
-    return (2*count) + 1
-  end
-
-  return 2*count
-end
-
+#Find the first ten digits of the sum of one-hundred 50-digit numbers
 def thirteen
   x = read_file_13
   x = x.to_s
@@ -464,6 +434,7 @@ def read_file_13
   return acc
 end
 
+#Fid the longest sequence using a starting number under one milion
 #fun problem with memoizing all at first hack, getting used to using ruby for code
 def fourteen
   max = 0
@@ -500,6 +471,7 @@ def collatz_helper(n, count)
 
 end 
 
+#Starting in the top left corner in a 20 by 20 grid, how many routes are there to the bottom right corner?
 #this solves but it takes too long... Notice that the solution is just Choose(2n, n)
 def fifteen 
   size_x = 11
@@ -579,6 +551,7 @@ def safe_mat?(x, y)
   return (((x < width) && (x >= 0) && (y < height) && (y >= 0)) && (@@mat[x][y] >= 0))
 end
 
+#What is the sum of the digits of the number 2^1000?
 def sixteen
   sum_of_digits 2**1000
 end
@@ -591,6 +564,7 @@ def sum_of_digits n
   return (n%10) + sum_of_digits(n / 10) 
 end
 
+#How many letters would be needed to write all the numbers in words from 1 to 1000?
 def seventeen
   acc = 0
   for i in 1..1000
@@ -679,6 +653,7 @@ def number_to_words_hash
   return num_hash
 end
 
+#Find the maximum sum travelling from the top of the triangle to the base.
 #Bottom up dynamic programming to find the maximum path in the tree, I tried top-down but it was more complicated and not worth the time.
 def eighteen
   @@triangle = read_file_18
@@ -746,6 +721,9 @@ def read_file_18
   return arr
 end
 
+#How many Sundays fell on the first of the month during the twentieth century?
+#This is an example of a problem that can be solved in ruby in < 15 lines but would take much longer in other 
+#languages that do not have such a large and robust standard library (.sunday? :-) )
 def nineteen
   count = 0
   for year in 1901..2000
@@ -758,10 +736,13 @@ def nineteen
   return count
 end
 
+#Find the sum of digits in 100!
 def twenty
   sum_of_digits(factorial(100))
 end
 
+#Another "cute" ruby trick, instead of writing out the factorial as a recursive 
+#function, just "inject" the * operator into the sequence!
 def factorial n
   if n == 0
     return 1
